a180d9315a8a18f2acf3e511b93c5d487ecbbd28
Kada Liao <kadaliao@gmail.com>
2020-09-27 23:45:40 +0800

    refact argument parsing

--- knowledge_exporter/__init__.py
+++ knowledge_exporter/__init__.py
@@ -1,9 +1,34 @@
+from argparse import ArgumentParser
+
+import click
+
 from .exporter import KnowledgeExporter
 from .geektime import GeekTime
 
+PROVIDERS = [GeekTime]
 
-def main():
-    app = KnowledgeExporter(GeekTime)
+provider_map = {provider.__name__: provider for provider in PROVIDERS}
+
+
+@click.command(help="📑 -> 📚 导出知识付费平台内容", no_args_is_help=True)
+@click.option(
+    "-t",
+    "--target",
+    type=click.Choice(provider_map.keys(), case_sensitive=False),
+    required=True,
+    help="知识付费平台",
+)
+@click.option("-u", "--username", required=True, help="手机号/用户名")
+@click.option("-p", "--password", required=True, help="密码")
+@click.option("--merge/--no-merge", default=True, help="合并专栏文章")
+@click.option("--headless/--head", default=True, help="使用无头浏览器", hidden=True)
+@click.option("--show-comments/--no-comments", default=True, help="显示文章评论")
+@click.argument("column_id", type=int, required=True)
+def main(**kwargs):
+    # def main(target, username, password, merge, headless, show_comments, column_id):
+    target = kwargs.pop("target")
+    exporter_class = provider_map[target]
+    app = KnowledgeExporter(exporter_class, **kwargs)
     app.run()
 
 
--- knowledge_exporter/exporter.py
+++ knowledge_exporter/exporter.py
@@ -8,35 +8,41 @@ from .utils import get_logger
 logger = get_logger(__name__)
 
 
-def get_args():
-    parser = ArgumentParser(description="下载知识付费专栏💸")
-    parser.add_argument("column_id", type=int, help="专栏ID", action="store", default=280)
-    parser.add_argument("--username", type=str, required=True, help="用户名/手机号")
-    parser.add_argument("--password", type=str, required=True, help="密码")
-    parser.add_argument(
-        "--no-headless", default=False, help="禁用无头模式", action="store_true"
-    )
-
-    args = parser.parse_args()
-
-    return args
-
-
 class KnowledgeExporter:
-    def __init__(self, exporter_class):
-        self._exporter_class = exporter_class
+    def __init__(
+        self,
+        exporter_class,
+        username,
+        password,
+        column_id,
+        show_comments=True,
+        merge=True,
+        headless=True,
+    ):
+        self.exporter_class = exporter_class
+        self.username = username
+        self.password = password
+        self.show_comments = show_comments
+        self.merge = merge
+        self.column_id = column_id
+        self.headless = headless
+
+        concur = 5 if headless else 1
+        self.semaphore = asyncio.Semaphore(concur)
 
     @property
     def Exporter(self):
-        return self._exporter_class(self.headless)
+        return self.exporter_class(self.headless)
 
-    async def coro(self, column_id):
+    async def coro(self):
         await self.Exporter.ensure_login(self.username, self.password)
 
         # 多等一会儿，免得后面打开的浏览器实例还未登录
         await asyncio.sleep(3)
 
-        column, chapters, articles = await self.Exporter.fetch_column_info(column_id)
+        column, chapters, articles = await self.Exporter.fetch_column_info(
+            self.column_id
+        )
 
         logger.info(f"📖 《{column.title}》，总共 {len(articles)} 文章需要下载！")
 
@@ -52,13 +58,4 @@ class KnowledgeExporter:
             tqdm.write(f"📄 已下载：{title}")
 
     def run(self):
-        args = get_args()
-
-        self.headless = not args.no_headless
-        self.username = args.username
-        self.password = args.password
-
-        concur = 5 if self.headless else 1
-        self.semaphore = asyncio.Semaphore(concur)
-
-        asyncio.get_event_loop().run_until_complete(self.coro(args.column_id))
+        asyncio.get_event_loop().run_until_complete(self.coro())
