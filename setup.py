07d667bee140273581779db6b6c08ede297b5ef2
Kada Liao <kadaliao@gmail.com>
2020-09-29 12:25:03 +0800

    limit to macOS temporarily

--- setup.py
+++ setup.py
@@ -26,6 +26,7 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
+        "Operating System :: MacOS",
         "Programming Language :: Python :: 3 :: Only",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
